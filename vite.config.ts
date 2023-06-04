7694c2ce17a5e84d50ede28ffd538a2435630ed7
Ivan Bakaidov <ibakaidov@ya.ru>
2023-06-04 15:52:09 +0300

    fix base fir gh

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ethics-of-ai/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
