fba89065b6d21a4a57b7a33aec3c5d7d6f61cbaf
Ivan Bakaidov <ibakaidov@ya.ru>
2023-06-04 13:13:39 +0300

    add larvov story

--- src/story/index.ts
+++ src/story/index.ts
@@ -2,12 +2,14 @@ import { Plot } from "./Plot";
 import { Story } from "./Story";
 import { firstChoosePlot } from "./plots/firstChoose";
 import { hHStory } from "./stories/HH";
+import { mFAStory } from "./stories/MFA";
 import { secretDreamStory } from "./stories/SecretDream";
 
 
 export const stories: Story[] = [
     secretDreamStory,
-    hHStory
+    hHStory, 
+    mFAStory
 ]
 
 export const plots: Plot[] = [
