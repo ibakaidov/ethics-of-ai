6aec8a0b0b0fe37e19f07894d27ed4fa3bb7661e
Ivan Bakaidov <ibakaidov@ya.ru>
2023-06-05 13:32:24 +0300

    remove base

--- src/router/index.ts
+++ src/router/index.ts
@@ -39,7 +39,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/ethics-of-ai/'),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
   
 })
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ethics-of-ai/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
